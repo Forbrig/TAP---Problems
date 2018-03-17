cda0c921ac5e15d9827bfd473cc9f1c178e9f84b
Forbrig <vitorforbrig@gmail.com>
2018-03-16 21:09:11 -0300

    start of uri 1409 knapsack

---
+++ uri/1409/1409.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main ( ) {
+  int m, n, g, k;
+
+  scanf("%d %d %d %d", &m, &n, &g, &k);
+  while (m != 0 && n != 0 && g != 0 && k != 0 ) {
+
+
+
+
+    scanf("%d %d %d %d", &m, &n, &g, &k);
+  }
+  printf("yaaaay\n");
+  return 0;
+}
