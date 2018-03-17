96771b66c157629c7addf0ec22f3d2edf0a63c29
Forbrig <vitorforbrig@gmail.com>
2018-03-16 21:00:22 -0300

    uri 1286 - knapsack

--- uri/1286/1286.c
+++ uri/1286/1286.c
@@ -1,60 +1,48 @@
 #include <stdio.h>
 
+int n; //quantity of items (pizzas in this case)
+
+int weight[20], value[20];
+
+//position of the pointer or number of items, capacity of the backpack
+int knapsack(int i, int capacity) {
+  int result, a, b;
+  if (i >= n) { //if has used all items
+    return 0;
+  } else if (weight[i] > capacity) { //if the current item is greater than the current capacity
+    return (knapsack(i + 1, capacity));
+  } else { //try the 2 available options, add the current item on the backpack or jump to the new
+    a = knapsack(i + 1, capacity);
+    b = value[i] + knapsack(i + 1, capacity - weight[i]);
+    if (a > b) { //select the best
+      result = a;
+    } else {
+      result = b;
+    }
+  }
+  return result;
+}
+
+
+
 int main ( ) {
   /*
   n = number of orders
   p = max number of pizzas allowed that time
   */
-  int n, p, pizzas[20], minutes[20], media[20], usable[20];
-  int i, localsum, globalsum, pzamount;
-  int aux, aux2, aux3, k, bubble;
+  int i, p;
 
   scanf("%d", &n);
 
   while (n != 0) {
     scanf("%d", &p);
     for (i = 0; i < n; i++) {
-      scanf("%d", &minutes[i]);
-      scanf("%d", &pizzas[i]);
-      usable[i] = 1;
-      media[i] = (minutes[i] / pizzas[i]); //average of minute / pizza
+      scanf("%d", &value[i]);
+      scanf("%d", &weight[i]);
     }
 
-    // sort by the average of minute / pizza
-    bubble = 1;
-    while (bubble != 0) {
-      bubble = 0;
-      for (k = 0; k < i; k++) {
-        if (media[k+1] < media[k] && k < (i-1)) {
-          aux = media[k];
-          aux2 = minutes[k];
-          aux3 = pizzas[k];
-
-          media[k] = media[k+1];
-          minutes[k] = minutes[k+1];
-          pizzas[k] = pizzas[k+1];
-
-          media[k+1] = aux;
-          minutes[k+1] = aux2;
-          pizzas[k+1] = aux3;
-
-          bubble++;
-        }
-      }
-    }
-
-    pzamount = localsum = globalsum = 0;
-    for (i = 0; i < n; i++) {
-      if (pzamount + pizzas[i] <= n) {
-        if (pizzas[i] / minutes[i] > globalsum) {
-          globalsum = minutes[i];
-          pzamount = pizzas[i];
-        }
-      }
-    }
-
+    printf("%d min.\n", knapsack(0, p));
     scanf("%d", &n);
   }
-
   return 0;
 }
